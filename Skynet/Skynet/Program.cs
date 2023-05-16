359fd1fa2d09cacc071ec89acad892b5e1259dfa
이상훈 <luke0103@naver.com>
2023-05-17 00:11:14 +0900

    [SKYN-2] Discord.Addons Nuget 추가

--- Skynet/Skynet/Program.cs
+++ Skynet/Skynet/Program.cs
@@ -3,13 +3,29 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Discord;
+using Discord.Commands;
+using Discord.WebSocket;
+using System.Reflection;
+using System.Diagnostics.Tracing;
+using Discord.Addons.Interactive;
 
 namespace Skynet
 {
     class Program
     {
-        static void Main(string[] args)
+        private DiscordSocketClient client;
+        private CommandService command;
+        private IServiceProvider services;
+
+        public async Task RunBotAsync()
         {
+            
+        }
+
+        private static void Main(string[] args)
+        {
+            
         }
     }
 }
