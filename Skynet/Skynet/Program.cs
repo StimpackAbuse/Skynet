9abb9ad59db77ff470927b8842daf37c87066bca
이상훈 <luke0103@naver.com>
2023-05-19 22:38:55 +0900

    [SKYN-2] Migration to .NET 6 - Discord not supporting .NET Framework 4.7.2

--- Skynet/Skynet/Program.cs
+++ Skynet/Skynet/Program.cs
@@ -12,6 +12,7 @@ using System.Diagnostics.Tracing;
 using Discord.Addons.Interactive;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Encryption;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace Skynet
 {
@@ -25,38 +26,107 @@ namespace Skynet
             public string token;
         }
 
-        private static string ENV_FILE = "SkyNet_env";
+        private static string ENV_FILE = "SkyNet_env.json";
+        private static string PREFIX = "#";
+
+        private string clientID = "";
+        private string clientSecret = "";
+        private string botToken = "";
 
         private DiscordSocketClient client;
         private CommandService command;
         private IServiceProvider services;
 
+        private Task ClientLog(LogMessage log)
+        {
+            Console.WriteLine(log);
+            return Task.CompletedTask;
+        }
+
+        #region Bot Initialization
         public async Task RunBotAsync()
         {
-            
+            await Task.Run(() => TokenInitialize().GetAwaiter().GetResult());
+
+            client = new DiscordSocketClient(new DiscordSocketConfig { WebSocketProvider = Discord.Net.Providers.WS4Net.WS4NetProvider.Instance });
+            command = new CommandService();
+            services = new ServiceCollection().AddSingleton(client).AddSingleton(command).AddSingleton<InteractiveService>().BuildServiceProvider();
+
+            client.Log += ClientLog;
+
+            await RegisterCommandsAsync();
+            await client.LoginAsync(TokenType.Bot, botToken);
+            await client.StartAsync();
+            await Task.Delay(-1);
         }
 
-        private static void Main(string[] args)
+        public async Task RegisterCommandsAsync()
         {
-            //최초 실행 시 
-            if (!File.Exists(Path.Combine(Directory.GetCurrentDirectory(), ENV_FILE)))
+            client.MessageReceived += HandleCommandAsync;
+            await command.AddModulesAsync(Assembly.GetEntryAssembly(), services);
+        }
+
+        public async Task HandleCommandAsync(SocketMessage arg)
+        {
+            SocketUserMessage msg = arg as SocketUserMessage;
+            SocketCommandContext context = new SocketCommandContext(client, msg);
+
+            int argPos = 0;
+
+            if (msg.Author.IsBot)
+                return;
+
+            if (msg.HasStringPrefix(PREFIX, ref argPos))
             {
-                Console.WriteLine("ENV FILE NOT FOUND - CONFIGURING MODE INITIATED");
-                Console.Write("CLIENT ID: ");
-                string id = Console.ReadLine();
-                Console.Write("CLIENT SECRET: ");
-                string secret = Console.ReadLine();
-                Console.Write("BOT TOKEN: ");
-                string token = Console.ReadLine();
+                var result = await command.ExecuteAsync(context, argPos, services);
 
-                ConfigureData data;
-                data.id = id;
-                data.secret = secret;
-                data.token = token;
-
-                string configureJson = JsonConvert.SerializeObject(data);
-                File.WriteAllText(Path.Combine(Directory.GetCurrentDirectory(), ENV_FILE), configureJson);
+                if (!result.IsSuccess)
+                    Console.WriteLine(result.ErrorReason);
             }
         }
+        #endregion
+
+        #region Token Initialization
+        /// <summary>
+        /// Call this function to create ENV file which contains Discord tokens.
+        /// </summary>
+        private async Task CreateTokenJSON()
+        {
+            Console.WriteLine("ENV FILE NOT FOUND - CONFIGURING MODE INITIATED");
+            Console.Write("CLIENT ID: ");
+            string id = await Task.Run(() => Console.ReadLine());
+            Console.Write("CLIENT SECRET: ");
+            string secret = await Task.Run(() => Console.ReadLine());
+            Console.Write("BOT TOKEN: ");
+            string token = await Task.Run(() => Console.ReadLine());
+
+            ConfigureData data;
+            data.id = id;
+            data.secret = secret;
+            data.token = token;
+            string configureJson = JsonConvert.SerializeObject(data);
+
+            File.WriteAllText(Path.Combine(Directory.GetCurrentDirectory(), ENV_FILE), configureJson);
+        }
+
+        /// <summary>
+        /// Token initialize function.
+        /// </summary>
+        private async Task TokenInitialize()
+        {
+            if (!File.Exists(Path.Combine(Directory.GetCurrentDirectory(), ENV_FILE)))
+            {
+                await CreateTokenJSON();
+            }
+
+            string tokenJson = File.ReadAllText(Path.Combine(Directory.GetCurrentDirectory(), ENV_FILE));
+            ConfigureData data = JsonConvert.DeserializeObject<ConfigureData>(tokenJson);
+            clientID = data.id;
+            clientSecret = data.secret;
+            botToken = data.token;
+        }
+        #endregion
+
+        private static void Main(string[] args) => new Program().RunBotAsync().GetAwaiter().GetResult();
     }
 }
